a89156802dbd2b457383e60c72e13b73766a9c59
Jonathan Kalambay <jonnyk20@gmail.com>
2017-06-27 20:55:51 +0200

    M

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,15 @@
-import { AngularTourOfHeroesPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
-describe('angular-tour-of-heroes App', () => {
-  let page: AngularTourOfHeroesPage;
+describe('QuickStart E2E Tests', function () {
 
-  beforeEach(() => {
-    page = new AngularTourOfHeroesPage();
+  let expectedMsg = 'Hello Angular';
+
+  beforeEach(function () {
+    browser.get('');
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  it('should display: ' + expectedMsg, function () {
+    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
   });
+
 });
